9889fb3b7d602aa6ac3674e4a72cb6bbb7202567
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-03-15 19:19:25 +0530

    ty

--- src/components/factory_walk/page.jsx
+++ src/components/factory_walk/page.jsx
@@ -27,7 +27,7 @@ const FactoryWalk = ({ loadFacoryWalk }) => {
 
   const variants = {
     hidden: { opacity: 0, y: 5 }, // Move the button down initially
-    visible: { opacity: 1, y: -100 }, // Move the button up to its original position
+    visible: { opacity: 1, y: -120 }, // Move the button up to its original position
   };
 
   useEffect(() => {
